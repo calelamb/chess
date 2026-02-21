3d7eab88e9a12b82309ff91338e2e51858143a2e
Cale Lamb <calejlamb@gmail.com>
2026-02-21 16:40:49 -0700

    add dataaccess package

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+/**
+ * DataAccess interface
+ */
+public interface DataAccess {
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    Collection<GameData> getGames() throws DataAccessException;
+
+    int createGame(GameData gData) throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+
+    void clear() throws DataAccessException;
+
+}
---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,65 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class MemoryDataAccess implements DataAccess {
+
+    private HashMap<String, UserData> users = new HashMap<>();
+    private HashMap<String, AuthData> authMap = new HashMap<>();
+    private HashMap<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        authMap.put(auth.authToken(), auth);
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authMap.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authMap.remove(authToken);
+    }
+
+    @Override
+    public Collection<GameData> getGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public int createGame(GameData gData) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        games.clear();
+        authMap.clear();
+        users.clear();
+    }
+}
