bfbea2afdd261d63d490c433fa41170a97199dff
Cale Lamb <calejlamb@gmail.com>
2026-02-22 21:47:38 -0700

    Finished RegisterService class

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,8 +1,11 @@
 package service;
 
 import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
 import model.AuthData;
 import model.UserData;
+
+import java.util.UUID;
 
 public class RegisterService {
 
@@ -12,8 +15,23 @@ public class RegisterService {
         this.data = d;
     }
 
-    public AuthData createNewUser(UserData d) throws {
-        return new AuthData(d.username(), );
+    public AuthData createNewUser(UserData d) throws BadRequestException, AlreadyTakenException, DataAccessException {
+        if (d.username() == null || d.password() == null || d.email() == null) {
+            throw new BadRequestException("One of more fields is empty");
+        } else if (d.username().isEmpty() || d.password().isEmpty() || d.email().isEmpty()) {
+            throw new BadRequestException("One or more fields has empty space");
+        }
+        if (data.getUser(d.username()) != null) {
+            throw new AlreadyTakenException("Username already taken");
+        }
+
+        data.createUser(d);
+        String token = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(d.username(), token);
+        data.createAuth(authData);
+
+        return authData;
     }
+
 
 }
