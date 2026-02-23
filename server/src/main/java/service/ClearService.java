9af7c6aaadc5c876187f131bc728e79127d0fe42
Cale Lamb <calejlamb@gmail.com>
2026-02-22 22:19:37 -0700

    More javadocs

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -3,6 +3,9 @@ package service;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 
+/**
+ * ClearService class for the Clear endpoint.
+ */
 public class ClearService {
 
     private DataAccess data;
@@ -11,6 +14,11 @@ public class ClearService {
         this.data = d;
     }
 
+    /**
+     * Clears the data from the DataAccess instance variable.
+     *
+     * @throws DataAccessException thrown if there's an error clearing the data
+     */
     public void clear() throws DataAccessException {
         data.clear();
     }
