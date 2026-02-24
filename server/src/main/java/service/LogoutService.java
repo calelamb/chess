183e7fe0cfbfd0324df3d8cc346b52a9281a1044
Cale Lamb <calejlamb@gmail.com>
2026-02-24 00:04:07 -0700

    logout service endpoint, implemented the endsession method

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.UserData;
+
 public class LogoutService {
+
+    private DataAccess data;
+
+    public LogoutService(DataAccess d) {
+        this.data = d;
+    }
+
+    /**
+     *
+     */
+    public void endSession(String authToken) throws DataAccessException, UnauthorizedException {
+        UserData token = data.getUser(authToken);
+        if (token != null) {
+            data.deleteAuth(authToken);
+        } else {
+            throw new UnauthorizedException("Auth token could not be verified");
+        }
+    }
 }
