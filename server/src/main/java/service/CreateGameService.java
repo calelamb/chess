29391f07b079a975caa81e9d6dd1d65b3ab02f07
Cale Lamb <calejlamb@gmail.com>
2026-02-24 13:23:37 -0700

    Created newGame method with token verification and new game creation.

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,8 +1,30 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+
 /**
  *
  */
 public class CreateGameService {
 
+    private final DataAccess data;
+
+    public CreateGameService(DataAccess d) {
+        this.data = d;
+    }
+
+    public int newGame(String authToken, String gameName) throws UnauthorizedException, DataAccessException {
+        AuthData existingAuth = data.getAuth(authToken);
+        if (existingAuth != null) {
+            int gID = data.createGame(new GameData(0, null, null, gameName, new ChessGame()));
+            return gID;
+        } else {
+            throw new UnauthorizedException("Auth token could not be verified");
+        }
+
+    }
 }
