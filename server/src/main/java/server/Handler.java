8d8960ba64a4745ea927c9ebe218b2d778cb9a10
Cale Lamb <calejlamb@gmail.com>
2026-02-24 17:47:30 -0700

    Started creating the Handler classes. Started with base Handler and wrote the method signature for deserialize helper method

---
+++ server/src/main/java/server/Handler.java
@@ -0,0 +1,15 @@
+package server;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ */
+public class Handler {
+
+    protected static final Gson gson = new Gson();
+
+    public <T>T deserialize(String json, Class<T> classType) {
+
+    }
+}
