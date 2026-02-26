b0104df6f9627646c19d036544121fcc6f059149
Cale Lamb <calejlamb@gmail.com>
2026-02-26 15:22:01 -0700

    Wrote the deserialize and serialize methods for the base handler class and started writing class and method level documentation

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -3,20 +3,30 @@ package server;
 import com.google.gson.Gson;
 
 /**
- *
+ * Base handler class that handles the serialization and deserialization of JSON objects using the GSON framework
  */
 public class Handler {
 
     protected static final Gson gson = new Gson();
 
     /**
+     * Takes in a JSON string object and deserializes the contained data to an arbitrary type defined by the class.
      *
-     * @param json
-     * @param classType
-     * @return
-     * @param <T>
+     * @param json      JSON String object
+     * @param classType The desired type to be cast
+     * @param <T>       Generic type T
+     * @return the deserialized data in an object of type T
      */
-    public <T>T deserialize(String json, Class<T> classType) {
+    public <T> T deserialize(String json, Class<T> classType) {
+        return gson.fromJson(json, classType);
+    }
 
+    /**
+     *
+     * @param o
+     * @return
+     */
+    public String serialize(Object o) {
+        return gson.toJson(o);
     }
 }
