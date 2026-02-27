387960b792b35285e85e1c21346e0e55eba7fd64
Cale Lamb <calejlamb@gmail.com>
2026-02-27 11:50:57 -0700

    Wrote documentation for LogoutHandler.java

--- server/src/main/java/server/LogoutHandler.java
+++ server/src/main/java/server/LogoutHandler.java
@@ -4,6 +4,9 @@ import io.javalin.http.Context;
 import service.LogoutService;
 import service.UnauthorizedException;
 
+/**
+ * Handles the HTTP DELETE request for the /session endpoint. Logs the user out by deleting the authorization data.
+ */
 public class LogoutHandler extends Handler {
 
     private final LogoutService l;
@@ -12,6 +15,12 @@ public class LogoutHandler extends Handler {
         this.l = l;
     }
 
+    /**
+     * Takes in a context header and ends the user's session by deleting the auth data.
+     *
+     * @param ctx Context object passed in
+     * @throws Exception thrown if there's an error accessing the auth token.
+     */
     public void handle(Context ctx) throws Exception {
         try {
             String header = ctx.header("authorization");
