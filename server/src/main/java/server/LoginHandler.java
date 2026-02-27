b849f053acfd51b33760c4ecf97c9016f46657c9
Cale Lamb <calejlamb@gmail.com>
2026-02-27 09:41:41 -0700

    Implemented error handling the LoginHandler class

--- server/src/main/java/server/LoginHandler.java
+++ server/src/main/java/server/LoginHandler.java
@@ -4,6 +4,7 @@ import io.javalin.http.Context;
 import model.AuthData;
 import model.UserData;
 import service.LoginService;
+import service.UnauthorizedException;
 
 /**
  *
@@ -16,12 +17,22 @@ public class LoginHandler extends Handler {
         this.l = l;
     }
 
+    /**
+     *
+     * @param ctx
+     * @throws Exception
+     */
     public void handle(Context ctx) throws Exception {
-        String body = ctx.body();
-        UserData userData = deserialize(body, UserData.class);
+        try {
+            String body = ctx.body();
+            UserData userData = deserialize(body, UserData.class);
 
-        AuthData newAuth = l.loginUser(userData);
-        ctx.status(200);
-        ctx.result(serialize(newAuth));
+            AuthData newAuth = l.loginUser(userData);
+            ctx.status(200);
+            ctx.result(serialize(newAuth));
+        } catch (UnauthorizedException e) {
+            ctx.status(401);
+            ctx.result(serialize(new ErrorMessage("Error: unauthorized")));
+        }
     }
 }
