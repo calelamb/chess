eb41a34caf737fb3bf0e03ea116c34841f483123
Cale Lamb <calejlamb@gmail.com>
2026-03-02 15:20:07 -0700

    Finished ListGamesHandler.java and wrote javadocs for it

--- server/src/main/java/server/ListGamesHandler.java
+++ server/src/main/java/server/ListGamesHandler.java
@@ -1,4 +1,43 @@
 package server;
 
-public class ListGamesHandler {
+import io.javalin.http.Context;
+import model.GameData;
+import service.ListGamesService;
+import service.UnauthorizedException;
+
+import java.util.Collection;
+
+/**
+ * Handles the HTTP GET request for the /game endpoint. Lists the current games under an authorization token
+ */
+public class ListGamesHandler extends Handler {
+
+    private final ListGamesService l;
+
+    private record ListGamesResponse(Collection<GameData> games) {
+    }
+
+    public ListGamesHandler(ListGamesService l) {
+        this.l = l;
+    }
+
+    /**
+     * Takes in a context header and lists the games on the current session
+     *
+     * @param ctx Context object passed in
+     * @throws Exception thrown if there's an error getting the games
+     */
+    public void handle(Context ctx) throws Exception {
+        try {
+            String header = ctx.header("authorization");
+            Collection<GameData> list = l.listGames(header);
+
+            ctx.status(200);
+            ctx.result(serialize(new ListGamesResponse(list)));
+
+        } catch (UnauthorizedException e) {
+            ctx.status(401);
+            ctx.result(serialize(new ErrorMessage("Error: unauthorized")));
+        }
+    }
 }
