499096563e27604cad505bb7191cbd867b694602
Cale Lamb <calejlamb@gmail.com>
2026-03-02 15:57:33 -0700

    fixed constructor

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,20 +2,23 @@ package server;
 
 import dataaccess.MemoryDataAccess;
 import io.javalin.*;
+import service.ClearService;
+import service.RegisterService;
 
 public class Server {
 
     private final Javalin javalin;
 
     private final MemoryDataAccess m;
-    var dataAccess = new MemoryDataAccess();
-    var clearHandler = new ClearHandler(new ClearService(dataAccess));
-    var registerHandler = new RegisterHandler(new RegisterService(dataAccess));
 
 
     public Server(MemoryDataAccess m) {
         this.m = m;
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        var dataAccess = new MemoryDataAccess();
+        var clearHandler = new ClearHandler(new ClearService(dataAccess));
+        var registerHandler = new RegisterHandler(new RegisterService(dataAccess));
 
         javalin.delete("/db", clearHandler::handle);
 
