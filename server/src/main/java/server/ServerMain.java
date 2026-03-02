b873b61cda789e46e898385ed9470e811bf76b00
Cale Lamb <calejlamb@gmail.com>
2026-03-02 16:20:35 -0700

    Initial testing, messed around with some errors that said the tests weren't in the project root. Passed 19/22

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -4,7 +4,6 @@ import chess.*;
 
 public class ServerMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        new Server().run(8080);
     }
 }
