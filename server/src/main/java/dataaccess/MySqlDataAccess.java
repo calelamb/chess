c8156cef3e21a9b10fb379bb133027e0e534f63a
Cale Lamb <calejlamb@gmail.com>
2026-03-11 19:53:03 -0600

    added interface method stubs to MySqlDataAccess

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,4 +1,64 @@
 package dataaccess;
 
-public class MySqlDataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySqlDataAccess implements DataAccess {
+    public MySqlDataAccess() {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public Collection<GameData> getGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public int createGame(GameData gData) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
