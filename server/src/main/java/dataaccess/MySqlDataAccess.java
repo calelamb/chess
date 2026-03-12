dd11d65f714a165cc1b79d26a3a20c980771d8e7
Cale Lamb <calejlamb@gmail.com>
2026-03-11 20:00:22 -0600

    wrote constructor and configure database method

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -4,12 +4,53 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
 
 public class MySqlDataAccess implements DataAccess {
-    public MySqlDataAccess() {
+    public MySqlDataAccess() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        configureDatabase();
+    }
 
+    private void configureDatabase() throws DataAccessException{
+        String[] statements = {
+                """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email    VARCHAR(255) NOT NULL,
+                PRIMARY KEY (username)
+            )
+            """,
+                """
+            CREATE TABLE IF NOT EXISTS auth (
+                authToken VARCHAR(255) NOT NULL,
+                username  VARCHAR(255) NOT NULL,
+                PRIMARY KEY (authToken)
+            )
+            """,
+                """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID        INT NOT NULL AUTO_INCREMENT,
+                whiteUsername VARCHAR(255),
+                blackUsername VARCHAR(255),
+                gameName      VARCHAR(255) NOT NULL,
+                game          TEXT NOT NULL,
+                PRIMARY KEY (gameID)
+            )
+            """
+        };
+        try (var conn = DatabaseManager.getConnection()) {
+            for (String statement : statements) {
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to configure database: " + e.getMessage());
+        }
     }
 
     @Override
