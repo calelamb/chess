15e6df3f716b4ddd4bcb91583208a2f6d03dc595
Cale Lamb <calejlamb@gmail.com>
2026-03-23 12:33:54 -0600

    Stubbed out a few methods

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,4 +1,17 @@
 package ui;
 
+import java.net.http.HttpClient;
+
 public class ServerFacade {
+
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public void registerUser () {
+
+    }
 }
