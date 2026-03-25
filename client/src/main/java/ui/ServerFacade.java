a6d835c99009d0066139bbe6a332b39dde91146d
Cale Lamb <calejlamb@gmail.com>
2026-03-25 16:41:35 -0600

    wrote out http helper

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,14 +1,25 @@
 package ui;
 
+import com.google.gson.Gson;
+import exception.AlreadyTakenException;
+import exception.BadRequestException;
+import exception.DataAccessException;
+import exception.UnauthorizedException;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class ServerFacade {
 
+    private final Gson GSON = new Gson();
     private final HttpClient client = HttpClient.newHttpClient();
     private final String serverUrl;
 
@@ -16,11 +27,55 @@ public class ServerFacade {
         this.serverUrl = serverUrl;
     }
 
+    private <T> T makeRequest(String method, String path, String authToken, Class<T> responseClass, Object body) throws URISyntaxException, IOException, InterruptedException, BadRequestException, UnauthorizedException, AlreadyTakenException, DataAccessException {
+        String fullUrl = serverUrl + path;
+        String jsonBody;
+
+        if (body != null) {
+            jsonBody = GSON.toJson(body);
+        } else {
+            jsonBody = "";
+        }
+
+        HttpRequest.Builder builder = HttpRequest.newBuilder().uri(new URI(fullUrl));
+        builder = builder.header("Content-Type", "application/json");
+        if (authToken != null) {
+            builder = builder.header("Authorization", authToken);
+        }
+        builder = builder.method(method, HttpRequest.BodyPublishers.ofString(jsonBody));
+        HttpRequest request = builder.build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        int status = response.statusCode();
+        switch (status) {
+            case 400:
+                throw new BadRequestException("Bad request");
+                break;
+
+            case 401:
+                throw new UnauthorizedException("Unauthorized");
+                break;
+
+            case 403:
+                throw new AlreadyTakenException("Already taken");
+                break;
+
+            case 500:
+                throw new DataAccessException("Error accessing the database");
+                break;
+
+            default:
+
+        }
+
+    }
+
     public AuthData registerUser(String username, String password, String email) {
         UserData ud = new UserData(username, password, email);
     }
 
     public AuthData loginUser(String username, String password) {
+
 
     }
 
