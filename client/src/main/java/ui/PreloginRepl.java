2780804befa3373699524ab60a631f7bf3f32777
Cale Lamb <calejlamb@gmail.com>
2026-03-27 15:29:20 -0600

    started PreloginRepl

--- client/src/main/java/ui/PreloginRepl.java
+++ client/src/main/java/ui/PreloginRepl.java
@@ -1,4 +1,17 @@
 package ui;
 
+import java.util.Scanner;
+
 public class PreloginRepl {
+    ServerFacade s;
+    Scanner scanner;
+
+    public PreloginRepl(ServerFacade s) {
+        this.s = s;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+
+    }
 }
