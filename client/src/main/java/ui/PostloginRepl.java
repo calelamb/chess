192e7cc7359c2f9c45c10bdf647e341b0e50586f
Cale Lamb <calejlamb@gmail.com>
2026-03-27 16:19:05 -0600

    Wrote out login repl logic, should be working

--- client/src/main/java/ui/PostloginRepl.java
+++ client/src/main/java/ui/PostloginRepl.java
@@ -1,4 +1,11 @@
 package ui;
 
+import model.AuthData;
+
 public class PostloginRepl {
+    public PostloginRepl(ServerFacade s, AuthData auth) {
+    }
+
+    public void run() {
+    }
 }
--- client/src/main/java/ui/PreloginRepl.java
+++ client/src/main/java/ui/PreloginRepl.java
@@ -1,5 +1,13 @@
 package ui;
 
+import exception.AlreadyTakenException;
+import exception.BadRequestException;
+import exception.DataAccessException;
+import exception.UnauthorizedException;
+import model.AuthData;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 
 public class PreloginRepl {
@@ -11,7 +19,46 @@ public class PreloginRepl {
         this.scanner = new Scanner(System.in);
     }
 
-    public void run() {
+    public void run() throws UnauthorizedException, BadRequestException, URISyntaxException, IOException,
+            InterruptedException, AlreadyTakenException, DataAccessException {
+        while (true) {
+            System.out.print("[LOGGED OUT] >>> ");
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case "help" -> {
+                    System.out.println("register <username> <password> <email>");
+                    System.out.println("login <username> <password>");
+                    System.out.println("quit");
+                    System.out.println("help");
+                }
+                case "quit" -> {
+                    return;
+                }
 
+                case "login" -> {
+                    try {
+                        AuthData auth = s.loginUser(tokens[1], tokens[2]);
+                        PostloginRepl pl = new PostloginRepl(s, auth);
+                        pl.run();
+                    } catch (Exception e) {
+                        System.out.print("error: " + e.getMessage());
+                    }
+                }
+
+                case "register" -> {
+                    try {
+                        AuthData auth = s.registerUser(tokens[1], tokens[2], tokens[3]);
+                        PostloginRepl pl = new PostloginRepl(s, auth);
+                        pl.run();
+                    } catch (Exception e) {
+                        System.out.print("error: " + e.getMessage());
+                    }
+                }
+
+                default -> System.out.print("Unknown Command");
+
+            }
+        }
     }
 }
