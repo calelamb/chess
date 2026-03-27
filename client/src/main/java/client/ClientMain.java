388a561d98cd87d777226c36c450b16528a46387
Cale Lamb <calejlamb@gmail.com>
2026-03-27 17:03:17 -0600

    wired up clientmain

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,21 @@
 package client;
 
 import chess.*;
+import exception.AlreadyTakenException;
+import exception.BadRequestException;
+import exception.DataAccessException;
+import exception.UnauthorizedException;
+import ui.PreloginRepl;
+import ui.ServerFacade;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class ClientMain {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+    public static void main(String[] args) throws UnauthorizedException, BadRequestException, URISyntaxException,
+            IOException, InterruptedException, AlreadyTakenException, DataAccessException {
+        ServerFacade sf = new ServerFacade("http://localhost:8000");
+        PreloginRepl pl = new PreloginRepl(sf);
+        pl.run();
     }
 }
