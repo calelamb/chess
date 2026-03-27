cfbeff4adf0ad2110a1c9d712aa1c09f6b357499
Cale Lamb <calejlamb@gmail.com>
2026-03-27 16:48:20 -0600

    working on logout repl and cases

--- client/src/main/java/ui/PostloginRepl.java
+++ client/src/main/java/ui/PostloginRepl.java
@@ -1,11 +1,58 @@
 package ui;
 
+import chess.ChessGame;
+import exception.AlreadyTakenException;
+import exception.BadRequestException;
+import exception.DataAccessException;
+import exception.UnauthorizedException;
 import model.AuthData;
+import model.GameData;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Scanner;
 
 public class PostloginRepl {
-    public PostloginRepl(ServerFacade s, AuthData auth) {
+    ServerFacade s;
+    Scanner scanner;
+    AuthData a;
+
+    public PostloginRepl(ServerFacade s, AuthData auth, Scanner scanner) {
+        this.s = s;
+        this.scanner = scanner;
+        this.a = auth;
     }
 
-    public void run() {
+    public void run() throws UnauthorizedException, BadRequestException, URISyntaxException,
+            IOException, InterruptedException, AlreadyTakenException, DataAccessException {
+        while (true) {
+            System.out.print("[LOGGED IN] >>>");
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case "help" -> {
+                    System.out.println("logout");
+                    System.out.println("list");
+                    System.out.println("create <gameName>");
+                    System.out.println("play <gameNumber> <WHITE|BLACK>");
+                    System.out.println("observe <gameNumber>");
+                    System.out.println("help");
+                }
+
+                case "logout" -> {
+                    s.logoutUser(a.authToken());
+                    return;
+                }
+
+                case "list" -> {
+                    Collection<GameData> games = s.listGames(a.authToken());
+                    int counter = 0;
+                    for (GameData game : games) {
+                        System.out.print(game.gameName());
+                    }
+                }
+            }
+        }
     }
 }
