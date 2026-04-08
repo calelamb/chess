323205583bbafdbdefdef67c1c06420945961ec2
Cale Lamb <calejlamb@gmail.com>
2026-04-08 14:47:20 -0600

    Started handleMessage helper method. Set to switch on command and push request to the server

--- server/src/main/java/server/WebSocket.java
+++ server/src/main/java/server/WebSocket.java
@@ -2,6 +2,7 @@ package server;
 
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
 
 import java.util.HashMap;
 
@@ -9,4 +10,27 @@ public class WebSocket {
 
     private HashMap<Integer, HashMap<Session, String>> sessions = new HashMap<>();
 
+    private void handleMessage(Session session, String jsonInput) {
+        UserGameCommand command = UserGameCommand.jsonToCommand(jsonInput);
+        UserGameCommand.CommandType commandType = command.getCommandType();
+
+        switch(commandType) {
+
+            case(UserGameCommand.CommandType.CONNECT) -> {
+
+            }
+
+            case(UserGameCommand.CommandType.MAKE_MOVE) -> {
+
+            }
+
+            case(UserGameCommand.CommandType.LEAVE) -> {
+
+            }
+
+            case(UserGameCommand.CommandType.RESIGN) -> {
+
+            }
+        }
+    }
 }
