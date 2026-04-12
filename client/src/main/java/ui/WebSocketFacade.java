24a3ee6680eb823ba3988a1234059cd74f11cc99
Cale Lamb <calejlamb@gmail.com>
2026-04-12 16:56:19 -0600

    wrote overwritten onOpen and connect method

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -1,11 +1,41 @@
 package ui;
 
-public class WebSocketFacade {
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import java.net.URI;
+
+public class WebSocketFacade extends Endpoint {
 
     private final String serverURL;
+    private final ServerMessageObserver obs;
+    private Session s;
 
-    public WebSocketFacade(String serverUrl, ServerMessageObserver observer, String serverURL) {
+    public WebSocketFacade(String url, ServerMessageObserver obs) {
 
-        this.serverURL = serverURL;
+        this.serverURL = url;
+        this.obs = obs;
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        s = session;
+        s.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String msg) {
+                obs.notifyMessage(msg, ServerMessage.jsonToMessage(msg));
+
+            }
+        });
+
+    }
+
+    public void connect() throws Exception {
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, new URI(serverURL));
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
